65040cda7bcb472adf2c5f45bb893e57bb2a7b91
matthiasjbraun <braun.matthias.j@gmail.com>
2024-06-18 20:26:18 +0200

    Final Get requests

--- aufgabe/data-endpoint/src/main/java/org/eclipse/edc/extension/data/DataApiController.java
+++ aufgabe/data-endpoint/src/main/java/org/eclipse/edc/extension/data/DataApiController.java
@@ -59,6 +59,58 @@ public class DataApiController {
         return gson.toJson(personList);
     }
 
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("data/id")
+    public String findPersonWithId(@QueryParam("id") String id)  {
+        monitor.info("Received a GET Query request");
+        Gson gson = new Gson();
+        Person person = dataManager.findPersonWithId(id);
+        return gson.toJson(person);
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("data/name")
+    public String findPersonWithName(@QueryParam("name") String name)  {
+        monitor.info("Received a GET Query request");
+        Gson gson = new Gson();
+        List<Person> personList = dataManager.findPersonWithName(name);
+        return gson.toJson(personList);
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("data/range")
+    public String findPersonsWithIdRange(@QueryParam("start") String start, @QueryParam("end") String end) {
+        monitor.info("Received a GET Query request");
+        Gson gson = new Gson();
+        List<Person> personList = dataManager.findPersonsWithIdRange(start, end);
+        return gson.toJson(personList);
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("data/company")
+    public String findPersonWithCompany(@QueryParam("name") String name, @QueryParam("catchPhrase") String catchPhrase,
+                             @QueryParam("bs") String bs) {
+        monitor.info("Received a GET Query request");
+        Gson gson = new Gson();
+        List<Person> personList = dataManager.findPersonWithCompany(name, catchPhrase, bs);
+        return gson.toJson(personList);
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("data/address")
+    public String findPersonWithAddress(@QueryParam("street") String street, @QueryParam("suite") String suite,
+                                        @QueryParam("city") String city, @QueryParam("zipcode") String zipcode) {
+        monitor.info("Received a GET Query request");
+        Gson gson = new Gson();
+        List<Person> personList = dataManager.findPersonWithAddress(street, suite, city, zipcode);
+        return gson.toJson(personList);
+    }
+
     @POST
     @Consumes({MediaType.APPLICATION_JSON})
     @Path("data")
@@ -67,6 +119,6 @@ public class DataApiController {
         Gson gson = new Gson();
         List<Person> personList = gson.fromJson(json, new TypeToken<List<Person>>(){}.getType());
         dataManager.persistPerson(personList);
-        return "Received Request" + personList.get(0).getName() + personList.get(1).getName();
+        return "Received Request, persisting Data";
     }
 }
--- aufgabe/database/src/main/java/database/DataManager.java
+++ aufgabe/database/src/main/java/database/DataManager.java
@@ -47,7 +47,7 @@ public class DataManager {
     }
 
     /**
-     * query the database for persons based on id
+     * query the database for a person based on id
      * @param id id of the Person
      * @return List of Person
      */
@@ -80,6 +80,59 @@ public class DataManager {
                 .setParameter(1, id)
                 .setParameter(2, name)
                 .setParameter(3, username)
+                .getResultList();
+    }
+
+    /**
+     * query the database for persons with Company
+     * @param name Company Name
+     * @param catchPhrase catchphrase of the Company
+     * @param bs bs of the Company
+     * @return List of Person
+     */
+    public List<Person> findPersonWithCompany(String name, String catchPhrase, String bs) {
+        EntityManager em = emf.createEntityManager();
+        return em.createQuery("SELECT p FROM Person p WHERE p.company.name = ?1"
+                        + "OR p.company.catchPhrase LIKE ?2"
+                        + "OR p.company.bs LIKE ?3", Person.class)
+                .setParameter(1, name)
+                .setParameter(2, catchPhrase)
+                .setParameter(3, bs)
+                .getResultList();
+    }
+
+    /**
+     * query the database for persons with Address
+     * @param street street of Address
+     * @param suite suite of Address
+     * @param city city of Address
+     * @param zipcode zipcode of Address
+     * @return List of Person
+     */
+    public List<Person> findPersonWithAddress(String street, String suite, String city, String zipcode) {
+        EntityManager em = emf.createEntityManager();
+        return em.createQuery("SELECT p FROM Person p WHERE p.address.street = ?1"
+                        + "OR p.address.suite LIKE ?2"
+                        + "OR p.address.city LIKE ?3"
+                        + "OR p.address.zipcode LIKE ?4", Person.class)
+                .setParameter(1, street)
+                .setParameter(2, suite)
+                .setParameter(3, city)
+                .setParameter(4, zipcode)
+                .getResultList();
+    }
+
+    /**
+     * query the database for persons within range of ids
+     * @param start id of the Person
+     * @param end name of the Person
+     * @return List of Person
+     */
+    public List<Person> findPersonsWithIdRange(String start, String end) {
+        EntityManager em = emf.createEntityManager();
+        return em.createQuery("SELECT p FROM Person p WHERE p.id BETWEEN ?1 AND ?2", Person.class)
+                .setParameter(1, start)
+                .setParameter(2, end)
                 .getResultList();
     }
 }
